4d6eccfb9cd19fe459c84a24d334446e46fe8e94
Itziar Otega <itziarortega94@gmail.com>
2020-01-14 17:58:30 +0100

    Add docs

---
+++ docs/precache-manifest.f33e68ec03ee7a468b64612b55808184.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "84275eade55d82cf7d3655d5670b7be2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5496eff75c5f873338f",
+    "url": "./static/css/main.8da936cb.chunk.css"
+  },
+  {
+    "revision": "fdd070d504cbc8e4a994",
+    "url": "./static/js/2.5a421e54.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "./static/js/2.5a421e54.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f5496eff75c5f873338f",
+    "url": "./static/js/main.ead27794.chunk.js"
+  },
+  {
+    "revision": "85c0b800d302ad589980",
+    "url": "./static/js/runtime-main.5c4b4e84.js"
+  },
+  {
+    "revision": "8017910cae86c3cc923ce51c83e7f4d0",
+    "url": "./static/media/back1.8017910c.jpg"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
